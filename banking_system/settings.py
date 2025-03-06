0deb904bd339cd09108992ac8a6e91beef711a68
AdeSam11 <adetoyinbosam99@gmail.com>
2025-03-06 01:28:59 +0100

    Modify settings

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -143,9 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / "static",
-    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Email Configurations
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
